863df974dceac6bb8a86fd8ffdffdadd62a9dbc0
Jon Betts <jon.betts@hypothes.is>
2020-08-31 14:43:42 +0100

    Add a non body consuming check for form boundaries
    This also adds an option to control whether blank content types should be
    assumed to be a form type for the purpose of checking when getting a form
    for the purposes of this check.
    
    The default is False, as otherwise installing `pyramid-sanity` could cause
    otherwise working apps to fail (e.g. ones expecting JSON, but not requiring
    a content type to declare it).

--- tests/unit/pyramid_sanity/tweens_test.py
+++ tests/unit/pyramid_sanity/tweens_test.py
@@ -31,12 +31,69 @@ class SharedTests:
 
 
 class TestInvalidFormTween(SharedTests):
-    def test_it_returns_InvalidFormData_for_invalid_form_post_requests(self, tween):
-        req = Request.blank("/", method="POST", content_type="multipart/form-data")
+    def test_it_returns_InvalidFormData_for_invalid_form_post_requests(
+        self, tween, form_type
+    ):
+        self.assert_raises_for_content_type(tween, content_type=form_type)
+
+    @pytest.mark.usefixtures("assume_form_on")
+    def test_it_returns_InvalidFormData_for_invalid_blank_content_type_when_enabled(
+        self, tween
+    ):
+        self.assert_raises_for_content_type(tween, content_type="")
+
+    def test_it_does_not_consume_the_post_body_iterator(self, tween, form_type):
+        req = Request.blank(
+            "/",
+            method="POST",
+            content_type=f"{form_type}; boundary=valid-boundary-------",
+            POST="content",
+        )
+
+        tween(req)
+
+        # pylint: disable=compare-to-zero
+        assert req.body_file_raw.tell() == 0
+        assert req.body_file.tell() == 0
+
+    @pytest.mark.parametrize(
+        "option,value",
+        (
+            ("method", "GET"),
+            ("content_type", "other"),
+            # By default a blank content type is not assumed to be a form
+            ("content_type", ""),
+        ),
+    )
+    def test_it_does_nothing_for_other_requests(self, handler, tween, option, value):
+        options = dict(
+            method="POST", content_type="multipart/form-data; boundary=239487389475"
+        )
+        options[option] = value
+
+        req = Request.blank("/any", **options)
+
+        response = tween(req)
+
+        handler.assert_called_once_with(req)
+        assert response == handler.return_value
+
+    def assert_raises_for_content_type(self, tween, content_type):
+        req = Request.blank("/", method="POST", content_type=content_type)
 
         result = tween(req)
 
         assert isinstance(result, InvalidFormData)
+
+    @pytest.fixture(
+        params=["multipart/form-data", "application/x-www-form-urlencoded"],
+    )
+    def form_type(self, request):
+        return request.param
+
+    @pytest.fixture
+    def assume_form_on(self, registry):
+        registry.settings["pyramid_sanity.check_form.assume_form_on_blank"] = True
 
     @pytest.fixture
     def tween(self, handler, registry):
